7b6d4c918954f0eb88b9fdef32ce7529998644cb
va.fomin <va.fomin@tensor.ru>
2025-07-22 08:35:35 +0300

    1

---
+++ 3/3.5/4.py
@@ -0,0 +1,13 @@
+from datetime import date, datetime, timedelta, time
+pattern = '%d.%m.%Y %H:%M'
+d = datetime.strptime(input(), pattern)
+td = timedelta(hours=d.hour, minutes=d.minute)
+
+
+if d.weekday() < 5 and timedelta(hours=9) <= td <= timedelta(hours=21):
+    print(int((timedelta(hours=21)-td).total_seconds() // 60))
+elif d.weekday > 4 and timedelta(hours=10) <= td <= timedelta(hours=18):
+    print(int((timedelta(hours=18)-td).total_seconds()//60))
+else:
+    print('Магазин не работает')   
+

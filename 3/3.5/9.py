162a21072def9afe6c27393f3e33d9c1a73f5658
FominVA <VycheslavFA@yandex.ru>
2025-07-23 19:59:33 +0300

    Update 9.py

--- 3/3.5/9.py
+++ 3/3.5/9.py
@@ -9,3 +9,7 @@ days_out = relise - new_data
 
 if days_out > timedelta(days= 1):
     print(f'До выхода курса осталось: {days_out.days} дней и {days_out.seconds//3600} часов')
+elif days_out < timedelta(days= 1):
+    print(f'До выхода курса осталось: {days_out.seconds//3600} час и {(days_out.seconds//60)%60} минут')
+elif days_out < timedelta(days= 1) and (days_out.seconds//60)%60 == 0:
+    print(f'До выхода курса осталось: {days_out.seconds//3600} часа')

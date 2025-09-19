27af815ae7f2005c9926c54926c871f962822fdd
FominVA <VycheslavFA@yandex.ru>
2025-09-19 20:09:53 +0300

    1

--- 6/6.8/6.py
+++ 6/6.8/6.py
@@ -1,8 +1,14 @@
 from collections import Counter
 
+def func_max():
+    result = [(key, value) for key, value in data.items() if value == max(data.values())]
+    return result
+
+def func_min():
+    result = [(key, value) for key, value in data.items() if value == min(data.values())]
+    return result
+
 data = Counter('aksjaskfjsklfjdslkfjajfopewtoieqpwdpqworiiqjskanvmcxbmpewrqopkqwlmdzczmxvmvlnjpjqpkqzxvmbowiqeorewi')
-m = max(data.values())
-print(m)
-# print(list(filter(lambda x: max(x.value()), data.most_common())))
-# data.__dict__['min_value'] = lambda: min(data.values())
-# data.__dict__['max_value'] = lambda: max(data.values())
+
+data.__dict__['min_values'] = func_min
+data.__dict__['max_values'] = func_max
---
+++ 6/6.8/7.py
@@ -0,0 +1,17 @@
+from collections import Counter
+import csv
+
+with open('name_log.csv', 'r', encoding='utf-8') as file:
+    rows = list(csv.reader(file))
+    columns=['username','email','dtime']
+    rows = rows[1:]
+    result = []
+    for row in rows:
+        row = row[1]
+        result.append(row)
+    sorted_result = Counter(sorted(result, key=lambda x: x[0]))
+    
+    for email, count in sorted(sorted_result.items()):
+        print(f'{email}: {count}')
+
+  

34895413c8a96a1c693d2bbcdfe6bfcf67501883
FominVA <VycheslavFA@yandex.ru>
2025-10-16 20:49:26 +0300

    1

---
+++ 9_Функции/9.6/1.py
@@ -0,0 +1,13 @@
+
+def get_digits(number: int | float) -> list[int | float]:
+    result = []
+    if isinstance(number, int):
+        for i in str(number):
+            result.append(int(i))
+    if isinstance(number, float):
+        number = str(number).replace('.', '')
+        for i in number:
+            result.append(int(i))
+    return result
+    
+print(get_digits(16733))
---
+++ 9_Функции/9.6/2.py
@@ -0,0 +1,3 @@
+number = 13.2313
+number_s = str(number).replace('.', '')
+print(number_s)

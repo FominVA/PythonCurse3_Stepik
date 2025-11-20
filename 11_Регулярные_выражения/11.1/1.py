2ae96fba7649e92868102a366ed9b805dcbec83d
va.fomin <va.fomin@tensor.ru>
2025-11-20 09:07:39 +0300

    1

--- 11_Регулярные_выражения/11.1/1.py
+++ 11_Регулярные_выражения/11.1/1.py
@@ -1,22 +1,6 @@
 import re
+regex = (r'\bcat', r'cat\b', r'\bcat\b', r'\Bcat', r'cat\B', r'\Bcat\B')
 
-pattern = r'[sS].{4}k'
-
-strings = [
-    's.4k',
-    'stepik',
-    'S.{4}k',
-    's4k',
-    'S....k',
-    's....k',
-    'S4444k',
-    'Stepik',
-    's.{4}k',
-    'S.4k'
-]
-
-for string in strings:
-    if re.match(pattern, string):
-        print(f'Строка "{string}" \033[92mсоответствует\033[0m рег. выражению {pattern}')
-    else:
-        print(f'Строка "{string}" \033[91mне соответствует\033[0m рег. выражению {pattern}')
+s = "Some words contain the word cat: scat, ducati, alcatel, catalyst, worldcat and of course cat!"
+for pattern in regex:
+    print(len(re.findall(pattern, s)))
